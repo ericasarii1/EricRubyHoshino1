6e68724636b5a0f42bd753ba6e93829b131f453e
ericasarii1 <nang.tamvan@gmail.com>
2025-05-31 10:56:40 +0700

    Update misc.py

--- zeldris/modules/misc.py
+++ zeldris/modules/misc.py
@@ -355,8 +355,8 @@ def src(update: Update, _: CallbackContext) -> None:
             [
                 [
                     InlineKeyboardButton(
-                        text="GitHub repo",
-                        url="github.com/IDN-C-X/ZeldrisRobot",
+                        text="Ovanime Indonesia",
+                        url="https://t.me/Grup_Ovanime_Indo",
                     ),
                 ],
             ],
@@ -580,16 +580,16 @@ def paste(update: Update, context: CallbackContext):
 __help__ = """
 An "odds and ends" module for small, simple commands which don't really fit anywhere
 
-× /id: Get the current group id. If used by replying to a message, gets that user's id.
-× /info: Get information about a user.
-× /wiki : Search wikipedia articles.
-× /rmeme: Sends random meme scraped from reddit.
-× /ud <query> : Search stuffs in urban dictionary.
-× /wall <query> : Get random wallpapers directly from bot!
-× /reverse : Reverse searches image or stickers on google.
-× /paste: Saves replied content to `hastebin` and replies with a url.
-× /gdpr: Deletes your information from the bot's database. Private chats only.
-× /markdownhelp: Quick summary of how markdown works in telegram - can only be called in private chats.
+⋊ /id: Get the current group id. If used by replying to a message, gets that user's id.
+⋊ /info: Get information about a user.
+⋊ /wiki : Search wikipedia articles.
+⋊ /rmeme: Sends random meme scraped from reddit.
+⋊ /ud <query> : Search stuffs in urban dictionary.
+⋊ /wall <query> : Get random wallpapers directly from bot!
+⋊ /reverse : Reverse searches image or stickers on google.
+⋊ /paste: Saves replied content to `hastebin` and replies with a url.
+⋊ /gdpr: Deletes your information from the bot's database. Private chats only.
+⋊ /markdownhelp: Quick summary of how markdown works in telegram - can only be called in private chats.
 """
 
 __mod_name__ = "Miscs"
